481b8da94c88229e77826202da16c3e2c3d7cfa0
Varun Joshi <121236149+varunjoshi27@users.noreply.github.com>
2024-08-29 01:12:52 +0530

    Sum Program

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number you want the sum");
+        int n=sc.nextInt();
+
+        int sum=0;
+        for(int i=1;i<n;i++){
+            sum=sum+i;
+        }
+        System.out.println("The sum of the given numbers is:"+sum);
+    }
+}
